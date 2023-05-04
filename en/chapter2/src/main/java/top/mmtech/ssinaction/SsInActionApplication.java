7fbec93a9f3881e00aae4abb548d5e6b2749c7df
jipeng.dong <jipeng.dong@opay-inc.com>
2023-05-04 11:24:52 +0800

    chepter-2

---
+++ en/chapter2/src/main/java/top/mmtech/ssinaction/SsInActionApplication.java
@@ -0,0 +1,13 @@
+package top.mmtech.ssinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsInActionApplication.class, args);
+	}
+
+}
