7d25b156d7b1fc659b3703d307970faff96810ab
jipeng.dong <jipeng.dong@opay-inc.com>
2023-07-03 11:34:07 +0800

    feature<Chapter10>: cors example

--- en/chapter10/src/main/java/top/mmtech/ssinaction/config/SecurityConfig.java
+++ en/chapter10/src/main/java/top/mmtech/ssinaction/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package top.mmtech.ssinaction.config;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,47 +12,53 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import top.mmtech.ssinaction.csrf.ZoffyCsrfService;
 
 @Configuration
 public class SecurityConfig {
 
-        @Value("${spring.profiles.active}")
-        private String env;
+  @Value("${spring.profiles.active}")
+  private String env;
 
-        @Bean
-        public UserDetailsService userDetailsService() {
-                final var users = new InMemoryUserDetailsManager();
-                final var user1 = User.withUsername("ivan").password("123456")
-                                .authorities("ADMIN").build();
-                final var user2 = User.withUsername("zoffy").password("123456")
-                                .authorities("USER").build();
-                users.createUser(user1);
-                users.createUser(user2);
-                return users;
-        }
+  @Bean
+  public UserDetailsService userDetailsService() {
+    final var users = new InMemoryUserDetailsManager();
+    final var user1 = User.withUsername("ivan").password("123456").authorities("ADMIN").build();
+    final var user2 = User.withUsername("zoffy").password("123456").authorities("USER").build();
+    users.createUser(user1);
+    users.createUser(user2);
+    return users;
+  }
 
-        @Bean
-        public PasswordEncoder passwordEncoder() {
-                return NoOpPasswordEncoder.getInstance();
-        }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
 
-
-        @Bean
-        public SecurityFilterChain securityFilterChain(final HttpSecurity http,
-                        final ZoffyCsrfService zoffyCsrfService)
-                        throws Exception {
-                http.headers().frameOptions().disable();
-                http.csrf(c -> {
-                        c.ignoringRequestMatchers(new AntPathRequestMatcher(
-                                        "/h2-console/**"));
-                        c.csrfTokenRepository(zoffyCsrfService);
-                });
-                http.authorizeHttpRequests().requestMatchers(
-                                new AntPathRequestMatcher("/h2-console/**"))
-                                .permitAll();
-                http.formLogin().defaultSuccessUrl("/main", true);
-                http.authorizeHttpRequests().anyRequest().permitAll();
-                return http.build();
-        }
+  @Bean
+  public SecurityFilterChain securityFilterChain(
+      final HttpSecurity http, final ZoffyCsrfService zoffyCsrfService) throws Exception {
+    http.headers().frameOptions().disable();
+    http.csrf().disable();
+    /* Could use separate class and config properties to define the cors Config */
+    http.cors(
+        c -> {
+          CorsConfigurationSource source =
+              request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("localhost:8080", "127.0.0.1:8080"));
+                config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTION"));
+                return config;
+              };
+          c.configurationSource(source);
+        });
+    http.authorizeHttpRequests()
+        .requestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+        .permitAll();
+    http.formLogin().defaultSuccessUrl("/main", true);
+    http.authorizeHttpRequests().anyRequest().permitAll();
+    return http.build();
+  }
 }
--- en/chapter10/src/main/java/top/mmtech/ssinaction/controller/HomeController.java
+++ en/chapter10/src/main/java/top/mmtech/ssinaction/controller/HomeController.java
@@ -1,25 +1,41 @@
 package top.mmtech.ssinaction.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HomeController {
 
   @GetMapping("/main")
+  @ResponseBody
   public String home() {
     return "aaaaaaa";
   }
 
+  @GetMapping("/ivan")
+  public String main() {
+    return "main.html";
+  }
 
   @GetMapping("/hello")
+  @ResponseBody
   public String hello() {
     return "hello";
   }
 
-
   @GetMapping("/ciao")
+  @ResponseBody
   public String ciao() {
     return "ciao";
+  }
+
+  @PostMapping("/test")
+  @ResponseBody
+  @CrossOrigin("http://localhost:8080")
+  public String test() {
+    return "testtest";
   }
 }
